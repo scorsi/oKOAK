f0f60aadb6a60843b07c3b1a8bae54f8100ab309
scorsi <sylvain.corsini@gmail.com>
2018-01-30 22:49:08 +0100

    True ocaml main function

--- koak.ml
+++ koak.ml
@@ -1,6 +1,6 @@
 (* Main *)
 
-let main () =
+let _ =
     Hashtbl.add Parser.binop_precedence ':' 2;
     Hashtbl.add Parser.binop_precedence '<' 10;
     Hashtbl.add Parser.binop_precedence '+' 20;
@@ -10,5 +10,3 @@ let main () =
     print_string "cmd> "; flush stdout;
     let stream = Lexer.lex (Stream.of_channel stdin) in Parser.parse stream;
 ;;
-
-main ()
